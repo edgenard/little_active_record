5af1f8caa99ce1fb5514e37dfa4b6e3302236fe2
Emmanuel Genard <edgenard@gmail.com>
2015-05-31 17:50:39 -0400

    Add Readme

--- little_ar/lib/associatable2.rb
+++ little_ar/lib/associatable2.rb
@@ -18,7 +18,7 @@ module Associatable
       through_fk = send(through_fk_name) #cats.owner_id
       
       source_fk_name = source_options.foreign_key#house_id
-      source_fk = send(through_name).send(source_fk_name)#humans.house_id
+
       
       through_pk = through_options.primary_key.to_s
       source_pk = source_options.primary_key.to_s #id
@@ -37,7 +37,6 @@ module Associatable
 
       source_options.model_class.parse_all(result).first
       
-      #through.model_class.find(through.foreign_key).send(source_name)
     end
   end
 end
