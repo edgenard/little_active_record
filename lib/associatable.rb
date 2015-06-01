ede6036114d8eea5f34e635ba69615b4bc26099e
Emmanuel Genard <edgenard@gmail.com>
2015-06-01 15:59:43 -0400

    Remove unecessary tests and other superfluous code

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -60,6 +60,45 @@ module Associatable
   def assoc_options
     @assoc_options ||= {}
   end
+  
+  def has_one_through(name, through_name, source_name)
+    
+  
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]#human
+      #house
+      source_options = through_options.model_class.assoc_options[source_name] 
+      
+      source_table = source_options.model_class.table_name #houses
+      through_table = through_options.model_class.table_name #humans
+      
+      through_fk_name = through_options.foreign_key #owner_id
+      through_fk = send(through_fk_name) #cats.owner_id
+      
+      source_fk_name = source_options.foreign_key#house_id
+
+      
+      through_pk = through_options.primary_key.to_s
+      source_pk = source_options.primary_key.to_s #id
+
+      result = DBConnection.execute(<<-SQL, through_fk.to_s)
+      SELECT
+        #{source_table}.*
+      FROM
+        #{through_table}
+      JOIN
+        #{source_table} ON #{through_table}.#{source_fk_name} = #{source_table}.#{source_pk.to_s}
+      WHERE
+        #{through_table}.#{through_pk.to_s} = ?
+        
+      SQL
+
+      source_options.model_class.parse_all(result).first
+      
+    end
+  end
+  
+  
 end
 
 class SQLObject
